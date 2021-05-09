64e38ae4c01bad923811e3a4a4d0e02e634faafd
Noah <mcbeasty11.nh@icloud.com>
2021-05-09 16:21:15 -0500

    mysql db connected to node app

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // import express
 const express = require('express');
+// import mysql2
+const mysql = require('mysql2');
 
 // Add the PORT designation and the app expression
 const PORT = process.env.PORT || 3001;
@@ -8,6 +10,28 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// code that will connect the application to the MySQL database
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: '1AppleMac$',
+    database: 'election',
+  },
+  console.log('Connected to the election database.')
+);
+
+// query the database to test the connection
+/* the db object is using the query() method. 
+This method runs the SQL query and executes the callback 
+with all the resulting rows that match the query. */
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
