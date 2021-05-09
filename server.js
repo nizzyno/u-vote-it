7937c3ae4d143cec17f86a9179949dc4ba681e6c
Noah <mcbeasty11.nh@icloud.com>
2021-05-09 16:35:10 -0500

    database calls

--- server.js
+++ server.js
@@ -29,9 +29,47 @@ const db = mysql.createConnection(
 /* the db object is using the query() method. 
 This method runs the SQL query and executes the callback 
 with all the resulting rows that match the query. */
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-  console.log(rows);
-});
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//   console.log(rows);
+// });
+
+// GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id < 10`, (err, row) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(row);
+// });
+
+// Delete a candidate
+/* The DELETE statement has a question mark (?) that denotes a placeholder, 
+making this a prepared statement. A prepared statement can execute the same 
+SQL statements repeatedly using different values in place of the placeholder. */
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+// Create a candidate
+/* In the SQL command we use the INSERT INTO command for the candidates table 
+to add the values that are assigned to params. The four placeholders must match 
+the four values in params, so we must use an array.
+
+Because the candidates table includes four columns—id, first_name, last_name, 
+and industry_connected—we need four placeholders (?) for those four values. The values 
+in the params array must match the order of those placeholders. */
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//               VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
